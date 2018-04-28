986d0ebe603cfeb106991cb1645f2ed85b0b38c6
lsivak <larasanders@gmail.com>
2018-04-28 00:02:53 +0000

    added migrations

--- db/migrate/20180427234636_create_candies.rb
+++ db/migrate/20180427234636_create_candies.rb
@@ -1,8 +1,8 @@
 class CreateCandies < ActiveRecord::Migration[5.2]
   def change
     create_table :candies do |t|
-      t.string, :name
-      t.string, :taste
+      t.string :name
+      t.string :taste
       t.integer :cost
 
       t.timestamps
--- db/migrate/20180427235002_create_users.rb
+++ db/migrate/20180427235002_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.string :password_digest
-      t.boolean, :employee
+      t.boolean :employee
       t.string :default
       t.string :false
       t.integer :cash
