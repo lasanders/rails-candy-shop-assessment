f80cd22d6d0b66b9f7d71ade0d6a9c154bffc446
lsivak <larasanders@gmail.com>
2018-04-28 21:10:50 +0000

    migrated tables - fixed users - hopefully it will allow me to push to github

--- db/migrate/20180427235002_create_users.rb
+++ db/migrate/20180427235002_create_users.rb
@@ -3,9 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.string :password_digest
-      t.boolean :employee
-      t.string :default
-      t.string :false
+      t.boolean :employee, default: false
       t.integer :cash
 
       t.timestamps
