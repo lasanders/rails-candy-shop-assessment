e4dc63a33ecab12cf7b03116cbb75de972d1817e
lsivak <larasanders@gmail.com>
2018-04-29 18:31:02 +0000

    created views pages. created form pages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ before_action :set_user, only: [:show, :edit, :update, :destroy]
     end
     
     def user_params
-        params.require(:user).permit(:name, :taste, :cost, :appetite)
+        params.require(:user).permit(:name, :taste, :cash, :appetite)
     end
 
 end
